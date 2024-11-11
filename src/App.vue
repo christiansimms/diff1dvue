2be25fadf665f4f722172047335822d01e47c37d
Christian Simms <csimms@botonomy.com>
2024-11-11 18:47:59 -0500

    Found the limit of tests.

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,14 @@ import Matcher from './components/Matcher.vue';
   </div>
 <!--    <Matcher :before="['a', null]"  :after="[null, null]"/>-->
 <!--  <Matcher :before="[null, null]"  :after="['a', null]"/>-->
-  <Matcher :before="['a', '']"  :after="['', 'a']" :before2="['', 'a', '']"  />
+<!--  <Matcher :before="['a', '']"  :after="['', 'a']" :before2="['', 'a', '']"  />-->
 
 <!--  <Matcher :before="['a', null, null]"  :after="[null, 'a', null]"/>-->
 <!--  <Matcher :before="['a', null]"  :after="[null, 'a']"/>-->
   <!-- Below is a conflict -->
 <!--    <Matcher :before="['a', 'a', null]"  :after="['a', null, 'a']"/>-->
 
-<!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['a', 'b', 'c', 'd', 'e']"/>-->
+  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['', 'a', 'b', 'c', 'd', 'e']" :before2="['d', '']"/>
 <!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['b', 'a', 'c', 'e', 'd']"/>-->
 <!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['e', 'd', 'c', 'b', 'a']"/>-->
 </template>
