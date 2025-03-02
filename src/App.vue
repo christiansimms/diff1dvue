3f73575890c03aa3a54260041011698927f4b0e5
Christian Simms <csimms@botonomy.com>
2025-03-02 14:59:10 -0500

    Play w/inputs.

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Matcher from './components/Matcher.vue';
   <div>
     <p>Given 2 arrays of characters, match and find diffs.</p>
   </div>
-<!--    <Matcher :before="['a', null]"  :after="[null, null]"/>-->
+    <Matcher :before="['a', null]"  :after="[null, null]"/>
 <!--  <Matcher :before="[null, null]"  :after="['a', null]"/>-->
 <!--  <Matcher :before="['a', '']"  :after="['', 'a']" :before2="['', 'a', '']"  />-->
 
@@ -15,7 +15,7 @@ import Matcher from './components/Matcher.vue';
   <!-- Below is a conflict -->
 <!--    <Matcher :before="['a', 'a', null]"  :after="['a', null, 'a']"/>-->
 
-  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['', 'a', 'b', 'c', 'd', 'e']" :before2="['d', '']"/>
+<!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['', 'a', 'b', 'c', 'd', 'e']" :before2="['d', '']"/>-->
 <!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['b', 'a', 'c', 'e', 'd']"/>-->
 <!--  <Matcher :before="['a', 'b', 'c', 'd', 'e']"  :after="['e', 'd', 'c', 'b', 'a']"/>-->
 </template>
